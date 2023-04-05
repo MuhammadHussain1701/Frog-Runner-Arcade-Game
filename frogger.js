2fa54c780f5569d9d329543cea89ecec7f2dc2b4
hussain <muhammadhussain170117@gmail.com>
2023-04-05 13:29:30 +0200

    Game is Completed .Just sound is left

--- frogger.js
+++ frogger.js
@@ -18,6 +18,8 @@ class Frogger{
             {
                 this.y-=grid
                 this.moving=true
+                this.frameX=1
+                this.frameY=0
             }
         }
         if(keys['ArrowDown']){
@@ -25,6 +27,7 @@ class Frogger{
             {
                 this.y+=grid
                 this.moving=true
+                this.frameY=3
             }
         }
         if(keys['ArrowLeft']){
@@ -32,13 +35,15 @@ class Frogger{
             {
                 this.x-=grid
                 this.moving=true
+                this.frameY=2
             }
         }
         if(keys['ArrowRight']){
             if(this.moving===false&&this.x<canvas.width-this.width*2)
             {
                 this.x+=grid
-                this.moving=true   
+                this.moving=true
+                this.frameY=1   
             }
         }
         if(this.y<0)
@@ -47,11 +52,19 @@ class Frogger{
         }
     }
     draw(){
-        ctx3.fillStyle='green'
-        ctx3.fillRect(this.x,this.y,this.width,this.height)
+        // ctx3.fillStyle='green'
+        // ctx3.fillRect(this.x,this.y,this.width,this.height)
+        ctx3.drawImage(froggerSprite,this.frameX*this.spriteHeight,this.frameY*this.spriteHeight,this.spriteWidth,this.spriteHeight,this.x-25,this.y-25,this.width*2,this.height*2)
     }   
     jump(){
-
+        if(this.moving===false)
+        {
+            this.frameX=1
+        }
+        else if(this.frameX===1)
+        {
+            this.frameX=0
+        }
     }
 }
 const frogger=new Frogger()
--- obstacles.js
+++ obstacles.js
@@ -7,10 +7,41 @@ class Obstacles{
         this.height=height
         this.speed=speed
         this.type=type
+        this.frameX=0
+        this.frameY=0
+        this.randomise=Math.floor(Math.random()*30+30)
+        this.carType=(Math.floor(Math.random()*numberOfCars))
     }
     draw(){
-        ctx1.fillStyle='blue'
-        ctx1.fillRect(this.x,this.y,this.width,this.height)
+
+        if(this.type==='turtle')
+        {
+            if(frame%this.randomise===0)
+            {
+
+                if(this.frameX>=1)
+                {
+                    this.frameX=0
+                }
+                else{
+                    this.frameX++
+                }
+            }
+            // ctx1.fillRect(this.x,this.y,this.width,this.height)
+            // ctx1.drawImage(turtle,sx,sy,sw,sh,dx,dy,dw,dh)
+            ctx1.drawImage(turtle,this.frameX*70,this.frameY*70,70,70,this.x,this.y,this.width,this.height)
+            
+        }
+        else if(this.type==='log')
+        {
+            ctx1.drawImage(log,this.x,this.y,this.width,this.height)
+        }
+        else{
+            ctx2.drawImage(car,this.frameX*this.width,this.carType*this.height,grid*2,grid,this.x,this.y,this.width,this.height)
+        }
+
+        // ctx3.fillStyle='blue'
+        // ctx3.fillRect(this.x,this.y,this.width,this.height)
     }
     update(){
         this.x+=this.speed*gameSpeed
@@ -19,13 +50,15 @@ class Obstacles{
             if (this.x>canvas.width+this.width)
              {
                 this.x=0-this.width
+                this.carType=(Math.floor(Math.random()*numberOfCars))
             }
         }
         else{
-
+            this.frameX=1
             if (this.x<0-this.width)
             {
                 this.x=canvas.width+this.width
+                this.carType=(Math.floor(Math.random()*numberOfCars))
             }
         }
         
@@ -74,4 +107,39 @@ function handleObstacles()
        logsArray[i].draw()
         
     }
+
+    // COLLISION WITH CAR
+    for(let i=0;i<carsArray.length;i++)
+    {
+        if(collision(frogger,carsArray[i])){
+            ctx4.drawImage(collisions,0,100,100,100,frogger.x,frogger.y,50,50)
+            resetGame()
+        }
+    }
+    
+    // COLLISION WITH LOGS AND TURTLES
+
+    if(frogger.y<250&&frogger.y>100)
+    {
+        safe=false
+
+        for(let i=0;i<logsArray.length;i++){
+            if(collision(frogger,logsArray[i]))
+            {
+                frogger.x+=logsArray[i].speed
+                safe=true
+            }
+
+        }
+        if(!safe)
+        {
+            for(let i=0;i<30;i++)
+            {
+                rippleArray.unshift(new Particle(frogger.x,frogger.y))
+            }
+            resetGame()
+        }
+    }
+
+
 }
